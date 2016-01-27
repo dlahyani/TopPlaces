f2cace580a185a43a9a6a02c756a15587e223964
Gennadi Iosad <gena@lightricks.com>
2016-01-27 10:37:57 +0200

    add Xcode pros

---
+++ TopPlaces/TopPlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopPlaces
+//
+//  Created by Gennadi Iosad on 26/01/2016.
+//  Copyright © 2016 LightricksNoobsDepartment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
